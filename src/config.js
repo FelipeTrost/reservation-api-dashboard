ef4e7c23b9fa489b8f424ad43f657aa83b318fdf
Felipe Trost <felipe.trost@gmail.com>
2020-11-09 22:31:57 +0100

    Small error

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const api_url = 'https://reservation-api-gans.herokuapp.com';
-export const api_url = 'http://localhost:5000';
+export const api_url = 'https://reservation-api-gans.herokuapp.com';
+// export const api_url = 'http://localhost:5000';
